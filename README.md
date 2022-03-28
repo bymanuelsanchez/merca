17d8426a7a0872480da273f0a3d2538ef56ec658
Manuel Sánchez <bymanuelsanchez@gmail.com>
2022-03-28 18:56:05 +0200

    28/03 - to do list updated

--- README.md
+++ README.md
@@ -13,13 +13,15 @@
 - [x] Add link to buttons *remember target=blank
 - [x] Add inner page movement *preguntar Diego por DOWN
 - [x] Arreglar grid
-- [ ] Linkear los botones de Proyectos
+- [x] Add JS presupuesto
+- [ ] Add JS pro-card
+- [x] @media print
+- [ ] copy (Change lorem for real info)
+- [x] Linkear los botones de Proyectos
 - [ ] Revisar placing
-- [ ] Background for nav
+- [x] Background for nav
 - [ ] Eliminate button fot mobile
 - [ ] Burger menu for mobile
-- [ ] Add JS
-- [ ] Change lorem for real info
 
 # MERCA's design file:
 https://www.figma.com/file/COq1i5OsGOpzLNuQxgJl7m/Manuel-projectbeta?node-id=50%3A95
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,22 @@
 function updateResult() {
     const formValue = obtainValues();
     let finalResult;
+    finalResult = 0;
+
     if (formValue.serigrafia) {
         finalResult = formValue.cantidadCamiseta * 2;
     } else if (formValue.vinilo) {
         finalResult = formValue.cantidadCamiseta * 1.5;
-    }
+    };
+
+    if (formValue.sticker500) {
+        finalResult = finalResult + 50;
+    } else if (formValue.sticker1000) {
+        finalResult = finalResult + 100;
+    } else if (formValue.sticker1500) {
+        finalResult = finalResult + 150;
+    };
+
     document.getElementById('final-result').innerHTML = finalResult;
 }
 
@@ -13,7 +24,12 @@ function obtainValues() {
     const serigrafia = document.getElementById('serigrafia').checked;
     const vinilo = document.getElementById('vinilo').checked;
     const cantidadCamiseta = parseInt(document.getElementById('cantidad-camiseta').value);
+
+    const sticker500 = document.getElementById('500').checked;
+    const sticker1000 = document.getElementById('1000').checked;
+    const sticker1500 = document.getElementById('1500').checked;
+    
     return {
-        serigrafia, vinilo, cantidadCamiseta
+        serigrafia, vinilo, cantidadCamiseta, sticker500, sticker1000, sticker1500
     }
 }
